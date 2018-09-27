3312ad9169ee94dfd8b811e20344106f145c5da1
MBaucas <mjbaucas@gmail.com>
2018-09-27 14:17:36 -0400

    added intial buttons and exit functionality

---
+++ app/src/main/java/com/mjbaucas/naviapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.mjbaucas.naviapp;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button exitBtn = findViewById(R.id.exit_app);
+        exitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Exit PointNavi")
+                    .setMessage("Are you sure you want to exit?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    })
+                    .setCancelable(false);
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+    }
+}

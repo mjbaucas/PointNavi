1c33b7aef3b888736ee6de7e42f7cda73fe65cda
MBaucas <mjbaucas@gmail.com>
2018-10-07 23:23:48 -0400

    Added string values and attached to appropriate parts

--- app/src/main/java/com/mjbaucas/naviapp/UniversityActivity.java
+++ app/src/main/java/com/mjbaucas/naviapp/UniversityActivity.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
